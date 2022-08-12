4f81876c54fcd8d68b4257d86cf0c2f196e568e5
Sangmin Lee <66083363+GINK-SS@users.noreply.github.com>
2022-08-13 05:46:32 +0900

    2 beta
    수정사항: 테마 및 CSS 다듬어야 함

---
+++ Crypto_Tracker/src/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#8c7ae6",
+};
+
+export const darkTheme = {
+  bgColor: "#222",
+  textColor: "#eee",
+  accentColor: "#8c7ae6",
+};
